bab7b616d6724037e6c5e7b5f6bf6226c394d583
UBC Student <gzhou01@pender.students.cs.ubc.ca>
2024-11-14 21:56:33 -0800

    Initial Commit

---
+++ pa3/stats.cpp
@@ -0,0 +1,33 @@
+
+#include "stats.h"
+
+stats::stats(PNG& im) {
+    /* Your code here!! */
+}
+
+long stats::getSum(char channel, pair<int, int> ul, int dim) {
+    /* Your code here!! */
+    return 0;
+}
+
+long stats::getSumSq(char channel, pair<int, int> ul, int dim) {
+    /* Your code here!! */
+    return 0;
+}
+
+long stats::rectArea(int dim) {
+    /* Your code here!! */
+    return 0;
+}
+
+// given a rectangle, compute its sum of squared deviations from mean, over all color channels.
+// see written specification for a description of this function.
+double stats::getVar(pair<int, int> ul, int dim) {
+    /* Your code here!! */
+    return 0;
+}
+
+RGBAPixel stats::getAvg(pair<int, int> ul, int dim) {
+    /* Your code here!! */
+    return RGBAPixel();
+}
